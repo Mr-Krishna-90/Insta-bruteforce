9f701f0d3e0fd236dc089525cd551266fe3f3a6a
KʀᎥsʜɴᴀ <160280710+Mr-Krishna-90@users.noreply.github.com>
2024-02-24 08:01:29 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,65 @@
+
+
+
+## ***INSTA-BRUTEFORCE SCRIPT***
+
+## AVAILABLE ON :
+
+* Termux
+
+### TESTED ON :
+
+* Termux
+
+### REQUIREMENTS :
+* internet
+* external storage permission
+* 1gb ram
+
+## FEATURES :
+* [+] Password Crack tool !
+* [+] Updated maintainence !
+* [+] Easy for beginners !
+* [+] Working virus tool for termux !
+
+## INSTALLATION [Termux] :
+
+* `apt-get update -y`
+* `apt-get upgrade -y`
+* `pkg install python -y`
+* `pkg install git -y`
+* `git clone https://github.com/Mr-Krishna-90/Insta-bruteforce`
+* `pip install requests`
+* `pip install random`
+* `pip install instaloader`
+* `pip install subprocess`
+* `pip install rich`
+* `cd $HOME`
+* `ls`
+* `cd Insta-bruteforce`
+* `ls`
+* `python insta-bruteforce.py`
+```
+
+Password  : we
+The tool may be outdated and not at working condition.
+
+## Author
+### If there is an issue in this tool, report [here](https://github.com/Mr_Krishna_909
+/I_Picker/issues)
+
+<p style="color:purple"><b>Connect With Us:</b></p>
+
+``(Tap To Redirect)``
+
+[![Github](https://img.shields.io/badge/Instagram-INSTAGroup-blue?style=for-the-badge&logo=instagram)](https://ig.me/j/AbZC3b_u344hDJtI)
+[![Github](https://img.shields.io/badge/Instagram-INSTAPAGE-blue?style=for-the-badge&logo=instagram)](https://instagram.com/rach.itgamer999)
+[![Github](https://img.shields.io/badge/TELEGRAM-TgGroup-orange?style=for-the-badge&logo=telegram)](https://t.me/+GrRkWxyiROs4ZGU1)
+
+<h3 style="color:purple"> THANKS FOR USING OUR TOOL </h3>
+
+``© KRISHNA 🇮🇳``
+
+
+## WARNING : 
+***This tool is only for educational purpose. If you use this tool for other purposes except education we will not be responsible in such cases.***
---
+++ intro.py
@@ -0,0 +1,149 @@
+import os
+import json
+from getpass import getpass
+import time
+import subprocess as sub
+import random
+import requests
+import getpass
+import re
+from rich import print
+from rich.console import Console
+from rich.panel import Panel
+
+def find_word_in_file(url, word):
+    response = requests.get(url)
+    if response.status_code == 200:
+        file_content = response.text
+        if word in file_content:
+            os.system("clear")
+            print(Panel('''
+[bold white][[bold red]^[bold white]] [bold green] Author: Krishna & Ꭰᥲʀk Ꮮᴇᴀᴅᴇʀ 
+[bold white][[bold red]^[bold white]] [bold green] Github: github.com/Mr-Krishna-90 
+[bold white][[bold red]^[bold white]] [bold green] Telegram: https://t.me/+GrRkWxyiROs4ZGU1
+ '''))
+            print(f'[bold green]Your id[italic blue] {word} [italic green] Has SucessFully Accepted')
+            print(f"[bold white][[bold blue]✓[bold white]] [bold green]Login Sucess Full Your User Id : [italic green]{word}")
+            time.sleep(5)
+            pass
+            
+            #print(f"The word '{word}' was found in the file.")
+        else:
+            print(Panel('''
+[bold white][[bold red]^[bold white]] [bold green] Author: Krishna & Ꭰᥲʀk Ꮮᴇᴀᴅᴇʀ 
+[bold white][[bold red]^[bold white]] [bold green] Github: github.com/Mr-Krishna-90 
+[bold white][[bold red]^[bold white]] [bold green] Telegram: https://t.me/+GrRkWxyiROs4ZGU1
+ '''))
+            print(f"[bold green]You haven't Allowed/n[italic yellow] Your Id is [italic red] {word}[bold green] Contact Admin For Activation")
+            print(f"[bold blue]Send Him The Id [bold yellow] {word} [bold blue] For Activation")
+            os.system("xdg-open https://instagram.com/rach.itgamer999/")
+    else:
+        print(f"Failed to retrieve the file. Status code: {response.status_code}")
+url = "https://raw.githubusercontent.com/MR-S74RK/INSTA/main/.img/users.txt"  
+word_to_find = getpass.getuser() 
+
+find_word_in_file(url,word_to_find)
+
+
+# password+ banner
+os.system("clear")
+
+
+# BANNER educational purposes
+print(Panel( '''
+
+[bold red]●[bold yellow] ●[bold green] ●
+      .---.        .-----------
+     /     \  __  /    ------
+    / /     \(  )/    -----
+   //////   ' \/ `   ---
+  //// / // :    : ---
+ // /   /  /`    '--
+//          //..\\\
+
+       ====UU====UU====
+           '//||\\\`
+             ''``
+
+[bold red]Insta-Bruteforce by  Krishna & Ꭰᥲʀk Ꮮᴇᴀᴅᴇʀ                           
+'''))
+print(Panel('''
+[[bold white][[bold red]^[bold white]] [bold green] Author: Krishna & Ꭰᥲʀk Ꮮᴇᴀᴅᴇʀ 
+[bold white][[bold red]^[bold white]] [bold green] Github: github.com/Mr-Krishna-90 
+[bold white][[bold red]^[bold white]] [bold green] Telegram: https://t.me/+GrRkWxyiROs4ZGU1
+ '''))
+
+
+
+class bcolors:
+    BOLD = '\033[1m'
+    PURPLE = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[95m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    UNDERLINE = '\033[4m'
+
+
+def start():
+
+    sceltadisc = input(
+        "\033[93m\033[1m\n\n[!]Use this program  for educational purposes only? [y/n]: ")
+
+    if sceltadisc == "y":
+        print("\n")
+        os.system("clear") 
+        print(Panel('''
+[bold red]●[bold yellow] ●[bold green] ●
+      .---.        .-----------
+     /     \  __  /    ------
+    / /     \(  )/    -----
+   //////   ' \/ `   ---
+  //// / // :    : ---
+ // /   /  /`    '--
+//          //..\\\
+
+       ====UU====UU====
+           '//||\\\`
+             ''``
+
+[bold red]Insta-Bruteforce by Krishna & Ꭰᥲʀk Ꮮᴇᴀᴅᴇʀ
+
+                      
+'''))
+
+    else:
+        os.system("xdg-open 'https://instagram.com/rach.itgamer999/")
+        os.system("clear")
+        print("\t  [#] Insta rach.itgamer999\t")
+        print("\t  [#]Telegram: https://t.me/+GrRkWxyiROs4ZGU1\t")
+
+        exit()
+
+
+def acquisizione():
+    while True:
+        if veri_break == "si":
+            break
+        USER = ""
+        USER = input(
+            '\033[1m\033[92m[?]ENTER INSTAGRAM USERNAME FOR CRACK PASSWORD: ')
+        wl = input("[?]Enter the PassList along The Path: ")
+        sleepp = int(
+            input("\033[1m\033[91m\n[!]Type the sleep time for login(sec) (Rec:900): "))
+        while True:
+            sub.call("clear")
+            procedere = input("Username to bruteforce = "+USER+"\n\nWordlist = "+wl+"\n\nSleep time = " +
+                              str(sleepp)+bcolors.WARNING+"\n\nProoceding? [y/break/modify]: "+bcolors.ENDC)
+            if procedere == "y":
+                veri_break = "si"
+                break
+            elif procedere == "modify":
+                print("\nReturn...")
+                break
+            elif procedere == "break":
+                exit()
+            else:
+                pass
+
